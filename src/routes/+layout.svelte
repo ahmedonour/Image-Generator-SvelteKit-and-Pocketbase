7a9b021e3e2a88452ffe11b558d673ee4197857d
Ahmed Onour <ahmedonour.49@gmail.com>
2023-02-06 11:17:29 +0200

    ADDING: A FOOTER

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,24 @@
 <script>
-	import Navbar from "../components/home/navbar.svelte";
+	import Footer from "../components/footer.svelte";
+import Navbar from "../components/home/navbar.svelte";
     let className = 'center';
 </script>
 <main>
 <Navbar bind:className/>
 </main>
 <slot/>
+<footer>
+    <Footer />
+</footer>
 <style>
     main{
         display: grid;
         place-items: center;
+    }
+    footer{
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 </style>
